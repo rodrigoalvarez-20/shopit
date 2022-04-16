5863287589c8d92026bbdd2f7a616935523af8cd
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-04-16 13:12:00 -0500

    Compile script

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+cd ~/shopit
+javac -cp $CATALINA_HOME/lib/javax.ws.rs-api-2.0.1.jar:$CATALINA_HOME/lib/gson-2.3.1.jar:. business/Service.java
+rm WEB-INF/classes/business/*
+cp business/*.class WEB-INF/classes/business/.
+jar cvf Service.war WEB-INF META-INF
+sh $CATALINA_HOME/bin/catalina.sh stop
+rm $CATALINA_HOME/webapps/Service.war
+rm -rf $CATALINA_HOME/webapps/Service
+mv Service.war $CATALINA_HOME/webapps
+if [ $1 -eq debug ];
+then
+    sh $CATALINA_HOME/bin/catalina.sh run
+else
+    sh $CATALINA_HOME/bin/catalina.sh start
+fi
