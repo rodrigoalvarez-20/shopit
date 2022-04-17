135e65566e5fad4684a5ac9480146645f45d34df
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-04-17 13:29:49 -0500

    User profile

--- business/Service.java
+++ business/Service.java
@@ -27,7 +27,7 @@ import java.util.Random;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.DecodedJWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
